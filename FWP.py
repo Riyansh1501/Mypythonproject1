39d85f49528afa272640eda103065cb6f65efed3
Riyansh1501 <riyanshdullu2015@gmail.com>
2026-09-13 13:10:14 +0200

    Add files via upload

---
+++ FWP.py
@@ -0,0 +1,21 @@
+print('Hello World')
+
+playername=input("What is your name?")
+
+
+print("Hello", playername, "How are you?")
+
+
+
+
+
+print(playername, "We are going to play a game!")
+
+
+print('PPPPP', 'Y   Y', 'TTTTT', 'H   H', 'OOOOO', 'N   N')
+print('P   P', ' Y Y ', '  T  ', 'H   H', 'O   O', 'NN  N')
+print('PPPPP', '  Y  ', '  T  ', 'HHHHH', 'O   O', 'N N N')
+print('P    ', '  Y  ', '  T  ', 'H   H', 'O   O', 'N  NN')
+print('P    ', '  Y  ', '  T  ', 'H   H', 'OOOOO', 'N   N')
+print()
+print('is so cool')
